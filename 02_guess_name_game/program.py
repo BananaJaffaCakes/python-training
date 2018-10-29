d43a994f9e176feceb6811f94b1e46b5f9a825d5
gitUser01 <git@user.com>
2018-10-29 22:09:05 +0000

    First complete build

--- 02_guess_name_game/program.py
+++ 02_guess_name_game/program.py
@@ -1,3 +1,24 @@
-print('----------------')
+import random
+
+print('-----------------')
 print('GUESS THAT NUMBER')
-print('----------------')
+print('-----------------')
+
+player_name = input('Enter your player name: ')
+
+the_number = random.randint(1, 100)
+
+print('A random number between 1 and 100 has been generated, can you guess the number {}?'.format(player_name))
+
+player_guess = 0
+
+while player_guess != the_number:
+    player_guess = input('Enter your guess: ')
+    if player_guess = the_number:
+        print('Well done, the number was {}.'.format(the_number))
+    elif player_guess > the_number:
+        print('Sorry {0}, your guess of {1} was too HIGH, try again!'.format(player_name, player_guess))
+    else:
+        print('Sorry {0}, your guess of {1} was too LOW, try again!'.format(player_name, player_guess))
+
+print('Thank you for playing {}, the game is now ending!'.format(the_number))
