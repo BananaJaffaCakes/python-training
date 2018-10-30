fabd8025b7a926691cb8eeef0094967d8442afa0
gitUser01 <git@user.com>
2018-10-30 22:51:14 +0000

    Features implemented

--- 03_birthday_countdown/program.py
+++ 03_birthday_countdown/program.py
@@ -1,25 +1,44 @@
-
+import datetime
 
 def print_header():
-    pass
+    print('-----------------')
+    print('  BIRTHDAY APP')
+    print('-----------------')
+    print()
+    print('This app will calculate the difference in days between your birthday and today this year.')
+    print()
 
 
 def get_birthday():
-    pass
+    print('Please provide your birthday as follows,')
+    b_year = int(input('Year [YYYY]: '))
+    b_month = int(input('Month [MM]: '))
+    b_day = int(input('Day [DD]: '))
+
+    b_date = datetime.date(b_year, b_month, b_day)
+    print('You have provided a birthday of {}'.format(b_date))
+
+    return b_date
 
 
-def calculate_day_diff():
-    pass
+def calculate_day_diff(user_date, today_date):
+    datedelta = user_date - today_date
+    return datedelta
 
 
-def print_day_diff_message():
-    pass
+def print_day_diff_message(birthday):
+    if birthday > 0:
+        print('Your birthday is in {} days'.format(birthday))
+    elif birthday < 0:
+        print('Your birthday was {} days back'.format(birthday))
+    else:
+        print('Congrats, Happy Birthday!')
 
 
 def main():
     print_header()
     user_birthday = get_birthday()
-    today = None
+    today = datetime.date.today()
     day_diff = calculate_day_diff(user_birthday, today)
     print_day_diff_message(day_diff)
 
