dbb8e190192ad0e8897aed98883619d7ba5cdb5a
gitUser01 <git@user.com>
2018-10-30 22:36:07 +0000

    Structure in place

---
+++ 03_birthday_countdown/program.py
@@ -0,0 +1,27 @@
+
+
+def print_header():
+    pass
+
+
+def get_birthday():
+    pass
+
+
+def calculate_day_diff():
+    pass
+
+
+def print_day_diff_message():
+    pass
+
+
+def main():
+    print_header()
+    user_birthday = get_birthday()
+    today = None
+    day_diff = calculate_day_diff(user_birthday, today)
+    print_day_diff_message(day_diff)
+
+
+main()
