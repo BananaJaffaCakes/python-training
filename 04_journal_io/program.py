ba0b5eccc5b420287f8faf5e2d19cf633e83eabe
gitUser01 <git@user.com>
2018-10-31 23:37:56 +0000

    Initial commit

---
+++ 04_journal_io/program.py
@@ -0,0 +1,16 @@
+def main():
+    print_header()
+    run_event_loop()
+
+
+def print_header():
+    print('-----------------')
+    print('   JOURNAL APP')
+    print('-----------------')
+
+
+def run_event_loop():
+    print('Run events')
+
+
+main()
