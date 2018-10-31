03d4ee5dea872a4eb2a73c2eebd3664f06a90934
gitUser01 <git@user.com>
2018-10-31 23:47:13 +0000

    Create event loop

--- 04_journal_io/program.py
+++ 04_journal_io/program.py
@@ -10,7 +10,16 @@ def print_header():
 
 
 def run_event_loop():
-    print('Run events')
-
+    cmd = input('Journal Options, [L]ist entries, [A]dd entries, E[x]it journal: ')
+    cmd = cmd.lower().strip()
+    
+    if cmd == 'l':
+        print('Listing entries...')
+    elif cmd == 'a':
+        print('Adding entries...')
+    elif cmd == 'x':
+        print('Exiting journal...')
+    else:
+        print('Invalid option...')
 
 main()
