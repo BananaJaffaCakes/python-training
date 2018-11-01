d8405302e042bdb70090c40530ad79d8eba6105d
gitUser01 <git@user.com>
2018-11-01 00:09:19 +0000

    Add while loop and create variables outside

--- 04_journal_io/program.py
+++ 04_journal_io/program.py
@@ -11,23 +11,28 @@ def print_header():
 
 
 def run_event_loop():
-    cmd = input('Journal Options, [L]ist entries, [A]dd entries, E[x]it journal: ')
-    cmd = cmd.lower().strip()
     
     j_entries = []
+    cmd = None
+    
+    while cmd != 'x':
+        cmd = input('Journal Options, [L]ist entries, [A]dd entries, E[x]it journal: ')
+        cmd = cmd.lower().strip()
 
-    if cmd == 'l':
-        list_entries(j_entries)
-    elif cmd == 'a':
-        add_entries(j_entries)
-    elif cmd != 'x':
-        print('Invalid option...')
+        if cmd == 'l':
+            list_entries(j_entries)
+        elif cmd == 'a':
+            add_entries(j_entries)
+        elif cmd != 'x':
+            print("Invalid option '{}'".format(cmd))
 
 
 def list_entries(entries):
-
-    for (index, entry) in enumerate(entries):
+    reversed_entries = reversed(entries)
+    print('<journal entries start>')
+    for (index, entry) in enumerate(reversed_entries):
         print('[{}] {}'.format(index, entry))
+    print('<journal entries end>')
 
 def add_entries(entries):
     new_entry = input('Journal entry, <enter> to commit: ')
