97fced3f960c8c1f558cdf2911086100445fd2d3
gitUser01 <git@user.com>
2018-11-01 00:02:40 +0000

    removed else to create loop

--- 04_journal_io/program.py
+++ 04_journal_io/program.py
@@ -1,6 +1,7 @@
 def main():
     print_header()
     run_event_loop()
+    print('Exiting journal...')
 
 
 def print_header():
@@ -13,13 +14,27 @@ def run_event_loop():
     cmd = input('Journal Options, [L]ist entries, [A]dd entries, E[x]it journal: ')
     cmd = cmd.lower().strip()
     
+    j_entries = []
+
     if cmd == 'l':
-        print('Listing entries...')
+        list_entries(j_entries)
     elif cmd == 'a':
-        print('Adding entries...')
-    elif cmd == 'x':
-        print('Exiting journal...')
-    else:
+        add_entries(j_entries)
+    elif cmd != 'x':
         print('Invalid option...')
+
+
+def list_entries(entries):
+
+    for (index, entry) in enumerate(entries):
+        print('[{}] {}'.format(index, entry))
+
+def add_entries(entries):
+    new_entry = input('Journal entry, <enter> to commit: ')
+    updated_entries = entries.append(new_entry)
+
+    return updated_entries
+
+
 
 main()
