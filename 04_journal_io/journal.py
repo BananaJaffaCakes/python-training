0e2e81907d21eef425311ba8b05ccda50e2bee91
gitUser01 <44544977+BananaJaffaCakes@users.noreply.github.com>
2018-11-05 23:11:33 +0000

    Add os path for file save

--- 04_journal_io/journal.py
+++ 04_journal_io/journal.py
@@ -1,12 +1,17 @@
+import os
+
 def load(name):
     #TODO: take journal name and load file from disk
     journal = []
     return journal
 
 
-def save(journal):
-    #TODO: take journal save file to disk
-    pass
+def save(journal, name):
+    #create absolute path to new file
+    jrl_path = os.path.abspath(os.path.join('./jrl_files/' + name + '.jrl'))
+    print('File save location: {}'.format(jrl_path))
+    #take list and write each item to new line
+   
 
 
 def add_entry(journal, new_entry):
