2483421bcc407cddc3139870b0f864913b8dff3f
gitUser01 <44544977+BananaJaffaCakes@users.noreply.github.com>
2018-11-06 01:07:00 +0000

    Initial load logic

--- 04_journal_io/journal.py
+++ 04_journal_io/journal.py
@@ -1,18 +1,34 @@
 import os
 
 def load(name):
-    #TODO: take journal name and load file from disk
+    #initialize list
     journal = []
+
+    print('Loading {}...'.format(name))
+
+    #create absolute path to file
+    jrl_path = os.path.abspath(
+        os.path.join('./04_journal_io/jrl_files/' + name + '.jrl'))
+    print('File load location: {}'.format(jrl_path))
+
+    #open file and read each item to to list
+    with open(jrl_path) as j_load:
+        entries = j_load.readlines()
+        for line in entries:
+            journal.append(line.rstrip())
+
     return journal
 
 
 def save(journal, name):
     print('Saving journal...')
+
     #create absolute path to new file
     jrl_path = os.path.abspath(
         os.path.join('./04_journal_io/jrl_files/' + name + '.jrl'))
     print('File save location: {}'.format(jrl_path))
-    #take list and write each item to new line
+
+    #take entry and write each item to new line
     with open(jrl_path, 'w') as j_save:
         for entry in journal:
             j_save.write(entry + '\n')   
