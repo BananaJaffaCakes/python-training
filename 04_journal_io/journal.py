4c772f46905f9eda661900c5880cd8789938afa6
gitUser01 <44544977+BananaJaffaCakes@users.noreply.github.com>
2018-11-08 22:43:50 +0000

    Added Doc Strings

--- 04_journal_io/journal.py
+++ 04_journal_io/journal.py
@@ -1,6 +1,17 @@
+"""
+A module with methods to load, add to and save a journal (jrl) file.
+"""
+
 import os
 
 def load(name):
+    """
+    Opens and reads a journal file into a list.
+
+    :param name: Name of the journal to load.
+    :return: A list with journal entries.
+    """
+
     #initialize list
     journal = []
 
@@ -21,6 +32,14 @@ def load(name):
 
 
 def save(journal, name):
+    """
+    Saves a journal list by writing to a jrl file
+
+    :param journal: Journal list to be saved.
+    :param name: Name of the journal to use when saving jrl file.
+    :return: Nothing
+    """
+
     print('Saving journal...')
 
     #create absolute path to new file
@@ -35,6 +54,14 @@ def save(journal, name):
 
 
 def add_entry(journal, new_entry):
+    """
+    Updates a journal list by append
+
+    :param journal: Journal list to be have data added too.
+    :param new_entry: String containg new entry to be added.
+    :return: An updated list with the new entry appended.
+    """
+
     updated_journal = journal.append(new_entry)
     return updated_journal
 
