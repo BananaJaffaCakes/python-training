fbbfc954706c67235b30999700dffcda77570e09
gitUser01 <44544977+BananaJaffaCakes@users.noreply.github.com>
2018-11-11 19:06:55 +0000

    http address creation

--- 05_real_time_weather/program.py
+++ 05_real_time_weather/program.py
@@ -1,6 +1,8 @@
 def main():
     print_header()
-
+    location = get_user_location()
+    fetch_http(location)
+        
     #user input for weather location
     #create a http request to site with location
     #scrape page for weather info
@@ -10,6 +12,17 @@ def print_header():
     print('--------------------')
     print('    WEATHER-APP')
     print('--------------------')
+
+
+def get_user_location():
+    input_location = input('Weather Location: ')
+    location = input_location.lower().strip()
+    return location
+
+
+def fetch_http(location):
+    address = "https://www.wunderground.com/weather/gb/{}".format(location)
+    print('Fetching weather data from {}'.format(address))
 
 
 if(__name__ == '__main__'):
