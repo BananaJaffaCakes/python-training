f5f51d879ec7a2f9ffd0384f516147c14018a6ed
gitUser01 <44544977+BananaJaffaCakes@users.noreply.github.com>
2018-11-11 17:41:44 +0000

    Initial commit

---
+++ 05_real_time_weather/program.py
@@ -0,0 +1,16 @@
+def main():
+    print_header()
+
+    #user input for weather location
+    #create a http request to site with location
+    #scrape page for weather info
+
+
+def print_header():
+    print('--------------------')
+    print('    WEATHER-APP')
+    print('--------------------')
+
+
+if(__name__ == '__main__'):
+    main()
