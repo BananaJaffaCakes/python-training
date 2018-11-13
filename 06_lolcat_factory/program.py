ba11b1ee3141600f19726e9f6f24cf76fef816aa
gitUser01 <44544977+BananaJaffaCakes@users.noreply.github.com>
2018-11-13 22:59:00 +0000

    Initial commit

---
+++ 06_lolcat_factory/program.py
@@ -0,0 +1,19 @@
+import os
+
+def main():
+    print_header()
+    create_folder()
+    
+    #download cats
+    #save cat to folder
+
+
+def print_header():
+    print('------------------------')
+    print('     LOLCat Factory')
+    print('------------------------')
+
+
+def create_folder():
+    print(os.path.abspath())
+
